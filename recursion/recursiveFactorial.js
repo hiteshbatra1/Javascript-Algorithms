2c81163d0897376b4352121cc16eafae636bb7c3
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-01-26 19:30:07 +0530

    Add Recursive Factorial

---
+++ recursion/recursiveFactorial.js
@@ -0,0 +1,17 @@
+function recursiveFactorial(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * recursiveFactorial(n - 1);
+}
+console.log(recursiveFactorial(5));
+// Output : 120
+
+// function factorial(n) {
+//   let result = 1;
+//   for (let i = 2; i <= n; i++) {
+//     result = result * i;
+//   }
+//   return result;
+// }
+// console.log(factorial(5));
